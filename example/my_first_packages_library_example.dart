1a6468678ec3cc1be0b788ae507077517ebd125f
Sumitro Aji Prabowo <bowo@anakdesa.id>
2023-09-28 15:04:39 +0700

    initial commit

---
+++ example/my_first_packages_library_example.dart
@@ -0,0 +1,6 @@
+import 'package:my_first_packages_library/my_first_packages_library.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/my_first_packages_library_test.dart
@@ -0,0 +1,16 @@
+import 'package:my_first_packages_library/my_first_packages_library.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
